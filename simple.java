f8d00ec7cc6bc2eab96bd48cbbb3c9bfda473b59
binukm090 <65102665+binukm090@users.noreply.github.com>
2020-05-11 01:05:34 -1200

    simple.java

--- simple.java
+++ simple.java
@@ -1,7 +1,6 @@
 public class hello {
    public static void main(String[] args) {
       // prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-      
+      System.out.println("Hello, World"); 
       }
  }     
